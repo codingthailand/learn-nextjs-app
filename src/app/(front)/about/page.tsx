7d36d318624a5e97c1e755a8964e49a010ddd3cd
Akenarin Komkoon <codingthaialnd@gmail.com>
2026-05-06 21:51:04 +0700

    update

--- src/app/(front)/about/page.tsx
+++ src/app/(front)/about/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 // http://localhost:3000/about
 export default function AboutPage() {
   return (
     <div>
       <p>About Us</p>
+      <Link href="/">Home</Link>
     </div>
   );
 }
--- src/app/(front)/page.tsx
+++ src/app/(front)/page.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Link href="/">Home</Link> | <Link href="/about">About Us</Link> | <Link href="/sign-in">SignIn</Link>
+    <div>
+      <Link href="/">Home</Link> | <Link href="/about">About Us</Link> | <Link href="/sign-in">Sign In</Link>
     </div>
   );
 }
